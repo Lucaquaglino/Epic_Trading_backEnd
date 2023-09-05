193f2b95c10f53163bbeb86a5502a3bbd822189b
Luca Quaglino <quagli.luca@gmail.com>
2023-09-05 02:32:51 +0200

    set up project

---
+++ EpicTrading/src/main/java/EpicTrading/EpicTradingApplication.java
@@ -0,0 +1,13 @@
+package EpicTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicTradingApplication.class, args);
+	}
+
+}
---
+++ EpicTrading/src/test/java/EpicTrading/EpicTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package EpicTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
